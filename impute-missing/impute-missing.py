a8d57f7077d1065f62590d1a77cbd8ff56493d5f
thanhpopper <116150847+thanhpopper@users.noreply.github.com>
2026-03-18 10:42:43 +0700

    Update solution for impute-missing

--- impute-missing/impute-missing.py
+++ impute-missing/impute-missing.py
@@ -7,13 +7,10 @@ def impute_missing(X, strategy='mean'):
     # Write code here
     X = np.array(X)
     filled_X = np.zeros_like(X)
-    if X.ndim == 1:
-        if strategy == 'mean':
-            fill_value = np.nanmean(X)
-        else:    
-            fill_value = np.nanmedian(X)
-        filled_X = np.where(np.isnan(X), fill_value, X)
-        return filled_X
+    dim = X.ndim
+    if dim == 1:
+        filled_X = filled_X.reshape(-1,1)
+        X = X.reshape(-1,1)
     # Calculate mean of each Feature column
     for i in range(X.shape[1]):
         if np.isnan(X[:,i]).all():
@@ -23,5 +20,8 @@ def impute_missing(X, strategy='mean'):
         else:    
             fill_value = np.nanmedian(X[:,i])
         filled_X[:,i] = np.where(np.isnan(X[:,i]), fill_value, X[:,i])
+    if dim == 1:
+        filled_X = filled_X.flatten()
+        X = X.flatten()
     return filled_X
     # 
